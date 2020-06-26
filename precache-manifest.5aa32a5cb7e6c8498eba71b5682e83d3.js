1a909e8195209ca75e8d16a1790712ef0b03f735
amay-zingg <anastasiamay29@gmail.com>
2020-06-26 17:25:37 -0400

    Updates

---
+++ precache-manifest.5aa32a5cb7e6c8498eba71b5682e83d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6521928138a002f9ae93aa5d896fa89f",
+    "url": "/anastasia-may-project-5/index.html"
+  },
+  {
+    "revision": "9500401f23333e4da3ee",
+    "url": "/anastasia-may-project-5/static/css/main.7f7c7bae.chunk.css"
+  },
+  {
+    "revision": "2b4529d6d50e37c13780",
+    "url": "/anastasia-may-project-5/static/js/2.6616c2d7.chunk.js"
+  },
+  {
+    "revision": "5c089f23e2df3789046e9ad72288f501",
+    "url": "/anastasia-may-project-5/static/js/2.6616c2d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9500401f23333e4da3ee",
+    "url": "/anastasia-may-project-5/static/js/main.6187dc12.chunk.js"
+  },
+  {
+    "revision": "7bd11f158783c96feadc",
+    "url": "/anastasia-may-project-5/static/js/runtime-main.8f907dda.js"
+  }
+]);
