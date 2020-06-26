c2c8ff249a6457a4686db8e776234316fdd4659f
amay-zingg <anastasiamay29@gmail.com>
2020-06-26 17:14:14 -0400

    The final project!

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
-import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
+import { compose } from 'recompose';
+import { withScriptjs, GoogleMap, withGoogleMap, Marker, InfoWindow } from 'react-google-maps';
 import firebase from '../Firebase/index';
+import sprayicon from '../../icons/spraypaint.png';
+// * * * * MADE WITH FUNCTIONALITY HELP FROM THIS INCREDIBLE PERSON https://twitter.com/danditomaso * * * * //
 
-export class MapContainer extends Component {
+const MapWithAMarker = compose(withScriptjs, withGoogleMap)((props) => {
+	return (
+		<GoogleMap zoom={16.5} defaultCenter={{ lat: 43.6545, lng: -79.4015 }}>
+			{props.markers.map((marker) => {
+				return (
+					<Marker
+						key={marker.id}
+						data-id={marker.id}
+						onClick={() => props.onClick(marker)}
+						position={{ lat: marker.artPiece.lat, lng: marker.artPiece.lng }}
+						icon={{
+							url: sprayicon
+						}}
+					>
+						{props.selectedMarker.id === marker.id && (
+							<InfoWindow marker={marker} onClose={props.onInfoWindowClose} visible={true}>
+								<div className="infoWindow">
+									<div className="locationImage">
+										<img src={marker.artPiece.image} alt={marker.artPiece.imageAlt} />
+										<p className="lastUpdate">Photo Taken: {marker.artPiece.imageDate}</p>
+									</div>
+									<div className="locationInfo">
+										<h3>{marker.artPiece.title}</h3>
+										<p>{marker.artPiece.location}</p>
+										<p className="artist">{marker.artPiece.artist}</p>
+										<p className="artistSocial">
+											<a href={marker.artPiece.artistSocial}>
+												<i className="fab fa-instagram" />
+												<span class="visuallyhidden">Instagram (opens a new window)</span>
+											</a>
+										</p>
+									</div>
+								</div>
+							</InfoWindow>
+						)}
+					</Marker>
+				);
+			})}
+		</GoogleMap>
+	);
+});
+
+class MapContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-      markers: [], 
-      selectedMarker: null,
-      isOpen: false,
-    };
+			markers: [],
+			activeMarker: {},
+			selectedMarker: {},
+			showingInfoWindow: false
+		};
 	}
 
 	componentDidMount() {
@@ -20,123 +66,51 @@ export class MapContainer extends Component {
 
 			for (let key in data) {
 				markersList.push({
-          id: key,
+					id: key,
 					artPiece: data[key]
-        });
-      }
-
+				});
+			}
 			this.setState({
-        markers: markersList
-      });
+				markers: markersList
+			});
+		});
+	}
 
-      console.log(this.state.markers,`this.state.markers`);
-    });
-    
-  }; // * * * * END OF COMPONENT DID MOUNT
+	handleClick = (marker) => {
+		this.setState({
+			activeMarker: marker,
+			selectedMarker: marker,
+			showingInfoWindow: true
+		});
+	};
 
-  // handleClick = (marker, event) => {
-  //     // console.log(marker, `I was clicked`);
-  //     this.setState({ selectedMarker: marker})
-  //   }
+	onInfoWindowClose = () =>
+		this.setState({
+			activeMarker: null,
+			showingInfoWindow: false
+		});
 
-  handleToggleOpen = () => {
-      this.setState({
-          isOpen: true
-      });
-      console.log("click town");
-  }
-
-  handleToggleClose = () => {
-      this.setState({
-          isOpen: false
-      });
-  }
+	onMapClicked = () => {
+		if (this.state.showingInfoWindow)
+			this.setState({
+				activeMarker: null,
+				showingInfoWindow: false
+			});
+	};
 
 	render() {
-    const style = {
-      position: 'relative',
-      width: '100%',
-      height: '100%'
-    };
-    
 		return (
-      <Map  google={this.props.google} 
-            zoom={16.5} 
-            style={style} 
-            initialCenter={{ lat: 43.6545, lng: -79.4015 }}>
-				
-       {this.state.markers.map((marker) => {
-          return (
-          <Marker
-              key={marker.artPiece.id}
-              icon={{
-                    url: './icons/spraypaint.png'
-                  }}
-              position={{ 
-                lat: marker.artPiece.lat, 
-                lng: marker.artPiece.lng 
-              }}
-              onClick={() => this.handleToggleOpen()} 
-          >
-            { this.state.isOpen &&
-              <InfoWindow>
-                <div>
-                  <p>Hello</p>
-                </div>
-              </InfoWindow>}
-        
-          </Marker> 
-           ) 
-          {/* // END OF MARKER RETURN */}
-        })} 
-        {/* // END OF RETURN FOR MAP() */}
-    </Map> 
-    ); // END OF ENTIRE RETURN
-  }; // END OF ENTIRE RENDER
-  }; // END OF CLASS COMPONENT
+			<MapWithAMarker
+				selectedMarker={this.state.selectedMarker}
+				markers={this.state.markers}
+				onClick={this.handleClick}
+				googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAP312VeMpu7VGjijjL1XopDWWIL46vl7I&v=3.exp&libraries=geometry,drawing,places"
+				loadingElement={<div style={{ height: `100%` }} />}
+				containerElement={<div style={{ height: `80vh` }} />}
+				mapElement={<div style={{ height: `100%` }} />}
+			/>
+		);
+	}
+}
 
-export default GoogleApiWrapper({
-	apiKey: 'AIzaSyAP312VeMpu7VGjijjL1XopDWWIL46vl7I'
-})(MapContainer);
-
-          /* {selectedMarker && (
-              <InfoWindow 
-              position = {{
-                lat: selectedMarker.artPiece.lat, 
-                lng: selectedMarker.artPiece.lng 
-              }}
-              onCloseClick ={() => {
-                setSelectedMarker(null);
-              }}
-              > */
-            //     {/* I WANT THIS WINDOW TO OPEN IF THE SELECTED MARKER IS CLICKED */}
-            //     {/* I WANT THIS TO BE CLOSED IF ANOTHER MARKER IS SELECTED */}
-            //     {/* IF ARTISTSOCIAL = "N/A" THEN DONT SHOW P.ARTISTSOCIAL */}
-            //     {/* IF ITEM TITLE = "KENSINGTON BIKE RACK" SHOW BIKE ICON NOT SPRAY PAINT CAN */}
-            //     {/* <InfoWindow>
-            //           <div className="infoWindow">
-            //           <div className="locationImage">
-            //             <img src="./images/KensingtonCarAndWallArt.jpg" alt="Placeholder"/>
-            //             <p className="lastUpdate">Image Last Updated</p>
-            //           </div>
-            //           <div className="locationInfo">
-            //             <h3>Image Title</h3>
-            //             <p>Location Here</p>
-            //             <p>Artist Name Here</p>
-            //             <p className="artistSocial">
-            //                         <a href="#socialLinkHere">
-            //                         <i className="fab fa-instagram"></i>
-            //                               <span class="visuallyhidden">Instagram (opens a new window)</span>
-            //               <span class="handle">INSTAGRAM ACCOUNT</span>
-            //               </a>
-            //             </p>
-            //           </div>
-            //         </div>
-            //     </InfoWindow> */}
-            //   {/* </InfoWindow>
-            //   )}; */}
-            // {/* </Marker> */}
-         
-
-
- 
+export default MapContainer;
