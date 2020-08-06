eadb47191120404332172189a2f00ba7b9e2e9ff
amay-zingg <anastasiamay29@gmail.com>
2020-08-06 03:31:06 -0400

    Small Map Config Fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.scss';
 import Header from './Header.js';
 import MapContainer from './Map.js';
+import Slideshow from './Slideshow';
 import Footer from './Footer.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import bikeRackDrawing from '../images/KensingtonTitle.png';
@@ -18,7 +19,7 @@ function App() {
 				<Switch>
 					<main>
 						<Route path="/" component={Landing} />
-						<Route path="/" exact component={MapContainer} />
+						<Route path="/anastasia-may-project-5" exact component={MapContainer} />
 						<Route path="/slideshow" component={Slideshow} />
 					</main>
 				</Switch>
