28559e103b7c3a337b9f3fa12d53e1ca97c46bff
amay-zingg <anastasiamay29@gmail.com>
2020-08-12 17:32:56 -0400

    Landing page as a modal popup fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.scss';
 import Header from './Header.js';
 import MapContainer from './Map.js';
 import Slideshow from './Slideshow';
 import Footer from './Footer.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import bikeRackDrawing from '../images/KensingtonTitle.png';
 
-function closeIntro() {
-	document.getElementById('landing-overlay').style.visibility = 'hidden';
-}
-
-function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Header />
-				<Switch>
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Header />
 					<main>
-						<Landing />
-						<Route
-							path="https://amay-zingg.github.io/anastasia-may-project-5/"
-							exact
-							component={MapContainer}
-						/>
-						<Route path="/" exact component={MapContainer} />
-						<Route path="/slideshow" component={Slideshow} />
+						<Switch>
+							<Route path="/" exact component={MapContainer} />
+							<Route path="/slideshow" component={Slideshow} />
+						</Switch>
 					</main>
-				</Switch>
-				<Footer />
-			</div>
-		</Router>
-	);
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
-
-const Landing = () => (
-	<div className="landing-overlay" id="landing-overlay">
-		<div className="landing">
-			<img src={bikeRackDrawing} alt="Kensington Bike Rack Title Image" className="title-photo" />
-			<div className="intro">
-				<p>
-					Kensington Market is a multicultural destination in Toronto to walk around and explore. Join me on a
-					street art tour!
-				</p>
-			</div>
-			<button className="enter-site" onClick={closeIntro}>
-				Enter
-			</button>
-		</div>
-	</div>
-);
 
 export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,19 @@
+
+// <div className="landing-overlay" id="landing-overlay">
+//     <div className="landing">
+//         <button className="close-dialog" onClick={infoToggle}>
+//             <i class="fas fa-window-close" />
+//             <span className="visuallyhidden">Close Dialog</span>
+//         </button>
+//         <img src={bikeRackDrawing} alt="Kensington Bike Rack Title Image" className="title-photo" />
+//         <div className="intro">
+//             <p>
+//                 Kensington Market is a multicultural destination in Toronto to walk around and explore. Join me on a
+//                 street art tour!
+//             </p>
+//         </div>
+//         <button className="enter-site" onClick={infoToggle}>
+//             Enter
+//         </button>
+//     </div>
+// </div> 
